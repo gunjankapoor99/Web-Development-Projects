a35dafb6aa40e159a0de94b3728bbf190f3df83f
darklegend99 <darklegend1931@gmail.com>
2021-07-25 20:32:31 +0530

    Check if searched post exist

--- Personal Blogs Website/app.js
+++ Personal Blogs Website/app.js
@@ -43,9 +43,10 @@ app.post("/compose", function(req,res){
 })
 
 app.get("/posts/:postName", function(req,res){
-  const requestedTitle = req.params.postName;
+  const requestedTitle = _.lowerCase(req.params.postName);
   posts.forEach(function(post){
-    if(post.title === requestedTitle){
+    const storedTitle = _.lowerCase(post.title);
+    if(storedTitle === requestedTitle){
        console.log("Match Found");
     }
   }); 
