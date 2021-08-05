c8b7436c32d3a80b13ae4f09ceff367e76a3d326
darklegend99 <darklegend1931@gmail.com>
2021-08-05 20:00:07 +0530

    Final Commit

--- Blog Articles/server.js
+++ Blog Articles/server.js
@@ -1,38 +1,23 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const Article = require("./models/article");
-const methodOverride = require("method-override");
-const articleRouter = require("./routes/articles");
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
 
-const app = express();
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
 
-mongoose.connect("mongodb://localhost/blogArticle", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-});
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
-app.set("view engine", "ejs");
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
 
-app.use(express.urlencoded({
-    extended: false
-}));
-app.use(methodOverride("_method"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use('/articles', articleRouter)
 
-app.get("/", async (req, res) => {
-    const articles = await Article.find().sort({
-        createdAt: "desc"
-    });
-    res.render("articles/index", {
-        articles: articles
-    });
-});
-
-// "/articles" is the relative path to the article routes
-app.use("/articles", articleRouter);
-
-app.listen(3000, function () {
-    console.log("Server is listening at port 3000!");
-});
+app.listen(5000)
