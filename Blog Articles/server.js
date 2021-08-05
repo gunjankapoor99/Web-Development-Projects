252166ccf4ba1c34234b0ffd7b9f45b920e55072
darklegend99 <darklegend1931@gmail.com>
2021-08-05 14:51:21 +0530

    Complete blog article project

--- Blog Articles/server.js
+++ Blog Articles/server.js
@@ -1,30 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const ejs = require("ejs");
+const Article = require("./models/article");
+const methodOverride = require("method-override");
 const articleRouter = require("./routes/articles");
 
 const app = express();
 
-mongoose.connect("mongodb://localhost/blogArticle", {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost/blogArticle", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
 
 app.set("view engine", "ejs");
 
-app.use(express.urlencoded({extended: false}));
-// app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req, res){
-    const articles = [{
-        title: "Test Article",
-        createdAt: new Date(),
-        description: "Test Description"
-    }]
-    res.render("articles/index", {articles: articles});
+app.get("/", async (req, res) => {
+    const articles = await Article.find().sort({
+        createdAt: "desc"
+    });
+    res.render("articles/index", {
+        articles: articles
+    });
 });
 
 // "/articles" is the relative path to the article routes
 app.use("/articles", articleRouter);
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server is listening at port 3000!");
 });
