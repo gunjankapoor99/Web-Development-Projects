85d601f61615faee2c6e53067585010c79071751
darklegend99 <darklegend1931@gmail.com>
2021-08-06 13:09:58 +0530

    Add home, register and login page

--- Registration form/app.js
+++ Registration form/app.js
@@ -6,18 +6,26 @@ require("./db/connect")
 const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
-app.use(express.static("templates/views"));
+app.use(express.static("views"));
 app.set("view engine", "hbs");
-app.set("views", "templates/views");
-hbs.registerPartials("templates/partials");
+app.set("views", "views");
+hbs.registerPartials("partials");
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.render("index");
 });
 
 app.get("/register", (req,res) => {
     res.render("register");
-})
+});
+
+app.get("/login", (req,res) => {
+    res.render("login");
+});
+
+app.get("/admissionForm", (req,res) => {
+    res.render("admissionForm");
+});
 
 app.listen(port, function(){
     console.log(`Server is running at port ${port}`);
