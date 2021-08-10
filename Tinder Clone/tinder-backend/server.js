999f97afe1a2dbd3f6129be14421af7a1a18b0f1
darklegend99 <darklegend1931@gmail.com>
2021-08-11 01:27:32 +0530

    Complete backend

--- Tinder Clone/tinder-backend/server.js
+++ Tinder Clone/tinder-backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
+import Cors from "cors";
+
 import Cards from "./dbCards.js";
 
 // App Config
@@ -8,7 +10,8 @@ const port = process.env.PORT || 8001;
 const connection_url = "mongodb+srv://admin:tDuuTVOAQgvATfAi@cluster0.r8qri.mongodb.net/tinderdb?retryWrites=true&w=majority";
 
 // Middlewares
-
+app.use(express.json());
+app.use(Cors());
 
 // DB config
 mongoose.connect(connection_url, {
@@ -22,9 +25,18 @@ app.get("/", function(req, res){
     res.status(200).send("Hello Divya!");
 });
 
+app.get("/tinder/cards", (req, res) => {
+    const dbCard = req.body;
+    Cards.find((err, data) => {
+        if(err){
+            res.status(500).send(err);
+        } else{
+            res.status(200).send(data);
+        }
+    })
+});
 
-
-app.post("/tinder/card", (req, res) => {
+app.post("/tinder/cards", (req, res) => {
     const dbCard = req.body;
     Cards.create(dbCard, (err, data) => {
         if(err){
--- Tinder Clone/tinder-clone/src/TinderCards.js
+++ Tinder Clone/tinder-clone/src/TinderCards.js
@@ -1,20 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TinderCard from 'react-tinder-card'
 import "./tinderCards.css";
+import axios from "./axios";
 
 function TinderCards() {
     
 // eslint-disable-next-line
     const [people, setPeople] = useState([
-        {
-            name: "Elon Musk",
-            url: "https://i.pinimg.com/originals/00/09/f4/0009f44897c3d9f6eb740dea9ec11022.png"
-        },
-        {
-            name: "Jeff Bezos",
-            url: "https://www.macitynet.it/wp-content/uploads/2017/11/jeff-bezos.jpg"
-        }
+        // {
+        //     name: "Elon Musk",
+        //     url: "https://i.pinimg.com/originals/00/09/f4/0009f44897c3d9f6eb740dea9ec11022.png"
+        // },
+        // {
+        //     name: "Jeff Bezos",
+        //     url: "https://www.macitynet.it/wp-content/uploads/2017/11/jeff-bezos.jpg"
+        // }
     ]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const req = await axios.get("/tinder/cards");
+            setPeople(req.data);
+        }
+        fetchData();
+    }, [])
 
     const swipped = (direction, nameToDelete) => {
         console.log("removing: " + nameToDelete);
@@ -36,7 +45,7 @@ function TinderCards() {
                     onCardLeftScreen={() => outOfFrame(person.name)}
                 >
                     <div
-                     style={{backgroundImage: `url(${person.url})`}}
+                     style={{backgroundImage: `url(${person.imgUrl})`}}
                      className="card"
                     >
                      <h3>{person.name}</h3>
