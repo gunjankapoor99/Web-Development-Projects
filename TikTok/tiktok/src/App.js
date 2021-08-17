91b132ae7f0e0aee2c9767671347a01927d2acd7
darklegend99 <darklegend1931@gmail.com>
2021-08-17 18:07:35 +0530

    Add video component

--- TikTok/tiktok/src/App.js
+++ TikTok/tiktok/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Video from "./Video";
 
 function App() {
   return (
     <div className="app">
       <h1>TikTok Clone!</h1>
+
+      <div className="app__videos">
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+      </div>
     </div>
   );
 }
---
+++ TikTok/tiktok/src/Video.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Video.css"
+
+function Video() {
+    return (
+        <div className="video">
+            <video loop className="video__player" src="https://player.vimeo.com/external/517995439.sd.mp4?s=5238b20291d7b04e5eaa0417bbd7750c8d5d6191&profile_id=139&oauth2_token_id=57447761"></video>
+
+            {/* Video Footer */}
+            {/* Video Sidebar */}
+        </div>
+    )
+}
+
+export default Video
